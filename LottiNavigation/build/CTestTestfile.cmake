cb19c541e9c9be5088b5c27d0fb58b5ac4c8acb7
ChaejinE <joung6517@gmail.com>
2021-09-08 11:48:23 +0900

    Lotti develop

---
+++ LottiNavigation/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/cj/isaac-sim-prj/git_clone/Isaac-Sim-Warehouse/LottiNavigation/src
+# Build directory: /home/cj/isaac-sim-prj/git_clone/Isaac-Sim-Warehouse/LottiNavigation/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ LottiNavigation/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cj/isaac-sim-prj/git_clone/Isaac-Sim-Warehouse/LottiNavigation/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/cj/isaac-sim-prj/git_clone/Isaac-Sim-Warehouse/TurtleBotNavigation/lotto_map_ws/devel/lib:/opt/ros/melodic/lib:/usr/local/cuda-10.2/lib64'
+export PWD='/home/cj/isaac-sim-prj/git_clone/Isaac-Sim-Warehouse/LottiNavigation/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cj/isaac-sim-prj/git_clone/Isaac-Sim-Warehouse/LottiNavigation/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/cj/isaac-sim-prj/git_clone/Isaac-Sim-Warehouse/LottiNavigation/src:$ROS_PACKAGE_PATH"
