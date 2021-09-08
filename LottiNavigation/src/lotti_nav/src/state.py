0fb92fdd494ca7969a49a43a8017314c6c774fd9
ChaejinE <joung6517@gmail.com>
2021-09-08 12:07:35 +0900

    Fixed #16

--- LottiNavigation/src/lotti_nav/src/state.py
+++ LottiNavigation/src/lotti_nav/src/state.py
@@ -1,5 +1,14 @@
 from enum import IntEnum
 
 class StateManager(IntEnum):
-    def __init__(self):
-        pass
+    Drive: int = 0
+    Wait: int = 1
+    Recognition: int = 2
+    Load: int = 3
+
+if __name__ == "__main__":
+    manager = StateManager
+    print(manager.Drive == 0)
+    print(manager.Wait == 1)
+    print(manager.Recognition == 2)
+    print(manager.Load == 3)
