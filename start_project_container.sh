34774db7720629d1778b8d081fe402ec9fcd96be
ChaejinE <joung6517@gmail.com>
2021-09-13 17:32:16 +0900

    docker image version up

--- start_project_container.sh
+++ start_project_container.sh
@@ -7,5 +7,5 @@ sudo docker run --entrypoint ./runapp.sh --gpus all -e "ACCEPT_EULA=Y" --network
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v /etc/localtime:/etc/localtime:ro \
         -v $PWD/job_in_docker:/root/job_in_docker\
-        -e DISPLAY=unix${DISPLAY} lottoworld777/project:0.2
+        -e DISPLAY=unix${DISPLAY} lottoworld777/project:0.3
 # job_in_docker is a sharing folder for sharing local files, fodlers, etc..
