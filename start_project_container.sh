cc33d5b4a551c4a5a05833e73f67c19bcd010d2a
ChaejinE <joung6517@gmail.com>
2021-09-14 16:59:47 +0900

    update

--- start_project_container.sh
+++ start_project_container.sh
@@ -1,11 +1,15 @@
 #!bin/bash
 
 sudo chmod 777 ./job_in_docker
-sudo xhost +local:
+sudo xhost +local:root
 
 sudo docker run --entrypoint ./runapp.sh --gpus all -e "ACCEPT_EULA=Y" --network=host \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -v /etc/localtime:/etc/localtime:ro \
+        -v /etc/localtime:/etc/localtime:rw \
         -v $PWD/job_in_docker:/root/job_in_docker\
+	-e QT_X11_NO_MITSHM=1\
+	-e NVIDIA_VISIBLE_DEVICES=all\
+	-e NVIDIA_DRIVER_CAPABILITIES=all\
+	-e XAUTHORITY=/tmp/.docker.xauth \
         -e DISPLAY=unix${DISPLAY} lottoworld777/project:0.3
 # job_in_docker is a sharing folder for sharing local files, fodlers, etc..
